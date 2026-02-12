8974f5226397e3d9776f1b1f515d3a2a70dcd30f
Ay <ayobamioketona@gmail.com>
2026-02-12 12:35:41 +0100

    route resolution

--- server/index.js
+++ server/index.js
@@ -1,4 +1,46 @@
-import app from "./app.js";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import path from 'path'
+import { fileURLToPath } from 'url';
+import { createRouteHandler } from "uploadthing/express";
+import { uploadRouter } from "./uploadthing.js";
+import productsRouter from './controllers/products.js';
 
+
+dotenv.config();
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const pathname = path.join(__dirname, '..', 'dist')
+console.log("PAthnme", pathname)
+app.use(express.static(pathname))
+app.use(cors({ origin: true }))
+app.use(express.json());
+app.use(
+  "/api/uploadthing",
+  createRouteHandler({
+    router: uploadRouter,
+  }),
+);
+
+// Connect to MongoDB
+// mongoose.connect(process.env.MONGODB_URI)
+// .then(() => console.log('MongoDB connected'))
+// .catch(err => console.error(err));
+
+
+// --- Routes ---
+app.use('/api/products', productsRouter);
+
+
+app.use((req, res) => {
+  console.warn(`Unhandled route: ${req.method} ${req.path}`);
+  res.status(404).json({ error: 'Route not found' });
+});
+
+export default app;
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Zilux backend running on http://localhost:${PORT}`));
